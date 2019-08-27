d6feee457a131bffbe2717f4affdd921b914ca04
HinduC <hindunandhini@gmail.com>
2019-08-27 15:39:31 +0530

    Project1 Committing..

---
+++ src/test/java/Demo1.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	static
+	{
+		System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+	}
+	@Test
+	public void testOne() throws InterruptedException
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+		Thread.sleep(1000);
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		//driver.close();
+	}
+
+}

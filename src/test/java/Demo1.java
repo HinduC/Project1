e5cb89b15f06278a7f89896c2263cbdcbaaf04af
HinduC <hindunandhini@gmail.com>
2019-08-28 11:38:01 +0530

    Changes Committing

--- src/test/java/Demo1.java
+++ src/test/java/Demo1.java
@@ -1,16 +1,19 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class Demo1 {
 	static
 	{
 		System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+		System.setProperty("webdriver.gecko.driver","./driver/geckodriver.exe");
 	}
 	@Test
 	public void testOne() throws InterruptedException
 	{
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = new FirefoxDriver();
+		//WebDriver driver = new ChromeDriver();
 		driver.get("http://www.google.com");
 		Thread.sleep(1000);
 		System.out.println(driver.getTitle());
